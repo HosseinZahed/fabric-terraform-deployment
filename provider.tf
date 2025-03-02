3b6ae62af7261f2421ee454164cd9ea3cf83ee0e
Hossein Zahed <hzahed@outlook.com>
2025-03-02 16:35:24 +0100

    wip

--- provider.tf
+++ provider.tf
@@ -5,11 +5,12 @@ terraform {
       version = "~>3.0"
     }
   }
-  backend "azurerm" {
+  backend "azurerm" {    
     resource_group_name  = "rg-temp"
     storage_account_name = "stterraformfabricpoc "
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    subscription_id      = "37b62c00-a726-4a50-b593-04b93728d5f6"
   }
 }
 
