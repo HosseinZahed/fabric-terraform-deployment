55e1639cb008306e796770ad65d01a7056fde025
Hossein Zahed <hzahed@outlook.com>
2025-03-02 16:59:20 +0100

    wip

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
-  required_providers {
-    required_version = ">= 1.8, < 2.0"
+  required_version = ">= 1.8, < 2.0"
+  required_providers {    
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
