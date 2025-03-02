59548b0330065b104e2ca659468a4de58e6d4cf2
Hossein Zahed <hzahed@outlook.com>
2025-03-02 16:42:45 +0100

    wip

--- provider.tf
+++ provider.tf
@@ -9,8 +9,7 @@ terraform {
     resource_group_name  = "rg-temp"
     storage_account_name = "stterraformfabricpoc "
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    subscription_id      = "37b62c00-a726-4a50-b593-04b93728d5f6"
+    key                  = "terraform.tfstate" 
   }
 }
 
